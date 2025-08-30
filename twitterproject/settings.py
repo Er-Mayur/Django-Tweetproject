abbeb5d11c00b49ac1047c11ec1ed2c88bbc6ee9
Mayur Mahajan <mahajanmayur2111@gmail.com>
2025-08-30 14:27:15 +0530

    Host

--- twitterproject/settings.py
+++ twitterproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3d!2yen13z3%$r@rq)*@vd+qfij!)okuu6u6m7!$5!i24)s-uv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app"]  # required for Vercel
 
 
 # Application definition
